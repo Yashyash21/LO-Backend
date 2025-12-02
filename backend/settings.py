fd48b24343d46c8da3b02b353d13810fa5fda4cd
yashyash21 <yashdhawale17@gmail.com>
2025-12-02 08:20:55 +0530

    updates

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-v-dd=&4^sw4)-q@*vz!9q%$+4%xlm$&d*jfp7ss=z^*6q-sekq'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # --------------------------------------------
 # INSTALLED APPS
