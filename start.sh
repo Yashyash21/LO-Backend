e3de944f2852f15fa2a77b73aca29d36d1bf4174
yashyash21 <yashdhawale17@gmail.com>
2025-12-02 06:53:43 +0530

    updates

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Apply database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the Django app
+gunicorn backend.wsgi
